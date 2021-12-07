8e61a1672440679896685752379fd837644fb915
Ravinder Kumar <ravinder.hindwaves@gmail.com>
2021-12-07 12:24:41 +0530

    Design Changes

---
+++ trunk/CompWebApp/webapp/build/precache-manifest.f59da159f49ab910a92605174494e63b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f249c4a3e320780e68fe8fba72d322d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b032f8b6282270d5500",
+    "url": "/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "184070033e0e4edc554c",
+    "url": "/static/css/main.460d03be.chunk.css"
+  },
+  {
+    "revision": "9b032f8b6282270d5500",
+    "url": "/static/js/2.1ebc00f2.chunk.js"
+  },
+  {
+    "revision": "24b8054ddc1445462862104fe5fc876e",
+    "url": "/static/js/2.1ebc00f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184070033e0e4edc554c",
+    "url": "/static/js/main.16108d83.chunk.js"
+  },
+  {
+    "revision": "608e1bc1a4ba6cf85865",
+    "url": "/static/js/runtime-main.c9a91e67.js"
+  }
+]);
